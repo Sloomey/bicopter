ffdc1275885888d3c74c40d2885406466795168b
Sloomey <liam.dillon0308@gmail.com>
2019-11-25 23:18:56 -0800

    Added controller input for everything but sticks in device_inputs.py

--- src/base_pi/device_inputs.py
+++ src/base_pi/device_inputs.py
@@ -11,6 +11,16 @@ stop_program = False # Stops the program and drone
 keyboard_buttons = ['w', 'a', 's', 'd', 'f' , 'g', 'h', 'j', 'k', 'q'] # List of all buttons on keyboard being used
 mouse_buttons = ['left', 'middle', 'right'] # List of all mouse buttons being used
 
+""" List of all controller buttons used. First index in tuple is the name of the button,
+    second index is the actual button name known by the library,
+    third index is the number the library uses to know when it's being pressed. This has been setup for an xbox controller. """
+
+controller_buttons = [('RIGHT BUMPER', 'BTN_TR', 1), ('LEFT BUMPER', 'BTN_TL', 1),
+                      ('DPAD-UP', 'ABS_HAT0Y' , -1), ('DPAD-DOWN', 'ABS_HAT0Y', 1), ('DPAD-RIGHT', 'ABS_HAT0X', 1), ('DPAD-LEFT', 'ABS_HAT0X', -1),
+                      ('START BUTTON', 'BTN_START', 1), ('SELECT BUTTON', 'BTN_SELECT', 1),
+                      ('X-BUTTON', 'BTN_NORTH', 1), ('Y-BUTTON', 'BTN_WEST', 1), ('B-BUTTON', 'BTN_EAST', 1), ('A-BUTTON', 'BTN_SOUTH', 1),
+                      ('LEFT TRIGGER', 'ABS_Z', 1023), ('RIGHT TRIGGER', 'ABS_RZ', 1023)] 
+
 """ Function used to see what input the user is doing and if the input
     isn't a listed input it won't do anything. """
 def device_input(controller_used = False, keyboard_used = False, mouse_used = False):
@@ -71,8 +81,71 @@ def device_input(controller_used = False, keyboard_used = False, mouse_used = Fa
                     pass
             
     elif controller_used == True:
-        print('Controller input coming soon.')
-        stop_program = True
+        events = inputs.get_gamepad() # Gets input from controller
+        for event in events:
+            for count, button in enumerate(controller_buttons):
+                for case in switch(count):
+                    if case(0): #Right Bumper
+                        if event.code == button[1] and event.state == button[2]:
+                            print('Pressing {}...'.format(button[0]))
+                            """ Later function will be added later. """
+                    elif case(1): #Left Bumper
+                        if event.code == button[1] and event.state == button[2]:
+                            print('Pressing {}...'.format(button[0]))
+                            """ Later function will be added later. """
+                    elif case(2): #DPAD UP
+                        if event.code == button[1] and event.state == button[2]:
+                            print('Pressing {}...'.format(button[0]))
+                            """ Later function will be added later. """
+                    elif case(3): #DPAD DOWN
+                        if event.code == button[1] and event.state == button[2]:
+                            print('Pressing {}...'.format(button[0]))
+                            """ Later function will be added later. """
+                    elif case(4): #DPAD RIGHT
+                        if event.code == button[1] and event.state == button[2]:
+                            print('Pressing {}...'.format(button[0]))
+                            """ Later function will be added later. """
+                    elif case(5): #DPAD LEFT
+                        if event.code == button[1] and event.state == button[2]:
+                            print('Pressing {}...'.format(button[0]))
+                            """ Later function will be added later. """
+                    elif case(6): #START BUTTON
+                        if event.code == button[1] and event.state == button[2]:
+                            print('Pressing {}...'.format(button[0]))
+                            """ Later function will be added later. """
+                    elif case(7): #SELECT BUTTON
+                        if event.code == button[1] and event.state == button[2]:
+                            print('Pressing {}...'.format(button[0]))
+                            """ Later function will be added later. """
+                    elif case(8): #X BUTTON
+                        if event.code == button[1] and event.state == button[2]:
+                            print('Pressing {}...'.format(button[0]))
+                            """ Later function will be added later. """
+                    elif case(9): #Y BUTTON
+                        if event.code == button[1] and event.state == button[2]:
+                            print('Pressing {}...'.format(button[0]))
+                            """ Later function will be added later. """
+                    elif case(10): #B BUTTON
+                        if event.code == button[1] and event.state == button[2]:
+                            print('Pressing {}...'.format(button[0]))
+                            """ Later function will be added later. """
+                    elif case(11): #A BUTTON
+                        if event.code == button[1] and event.state == button[2]:
+                            print('Pressing {}...'.format(button[0]))
+                            """ Later function will be added later. """
+                    elif case(12): # LEFT TRIGGER
+                        if event.code == button[1] and event.state == button[2]:
+                            print('Pressing {}...'.format(button[0]))
+                            """ Later function will be added later. """
+                    elif case(13): # RIGHT TRIGGER
+                        if event.code == button[1] and event.state == button[2]:
+                            print('Pressing {}...'.format(button[0]))
+                            """ Later function will be added later. """
+                    else:
+                        pass
+                    
+                
+            
     else:
         print("Please Choose an input device. This can be achieved by typing 'controller_used = True' or 'keyboard_used = True' in the function's parameters.")
         stop_program = True
@@ -120,7 +193,7 @@ def device_input(controller_used = False, keyboard_used = False, mouse_used = Fa
 if __name__ == '__main__': # Only runs this code if you execute this file.
     while not stop_program:  
         try:
-            device_input(keyboard_used = True, mouse_used = True)
+            device_input(controller_used = True)
         except Exception as error:
             print(error)
             print('ERROR: EXITING')
