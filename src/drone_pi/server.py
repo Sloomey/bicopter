ad3925551cde0a3a74c3d136186cf8698e150ab0
Sloomey <liam.dillon0308@gmail.com>
2019-12-15 16:28:43 -0800

    Added closing the socket after user is done with drone.

--- src/drone_pi/server.py
+++ src/drone_pi/server.py
@@ -1,5 +1,3 @@
-""" This file is used to receive input wirelessly from the base pi."""
-
 import socket # Used for sending data wirelessly
 import json # Used to decode python objects
 import pi_control # Used to control drone
@@ -7,7 +5,7 @@ import time # Used for time
 
 listensocket = socket.socket() # Makes a socket on IPV4 using TCP
 PORT = 31415 # Port to host server on
-maxConnections = 5 # Max connections to host at a time
+maxConnections = 1 # Max connections to host at a time
 IP = socket.gethostname() # IP address of local machine
 
 listensocket.bind(('',PORT))
@@ -18,7 +16,7 @@ print(f'Server started at {IP} on port {PORT}')
 clientsocket, address = listensocket.accept() # Accepts the incomming connection
 print(f'Connection from {address} has been established!')
 
-while True:
+while pi_control.drone_off == False:
     try:
         controller_inputs = clientsocket.recv(1024).decode('utf-8') #Gets the incomming message
         msg = json.loads(controller_inputs)
@@ -28,3 +26,7 @@ while True:
             print(pi_control.use_input(msg))
     except json.decoder.JSONDecodeError:
         pass
+
+listensocket.shutdown(socket.SHUT_RDWR) # Shuts down socket
+listensocket.close() # Closes Socket
+print(f'Closing server at {IP} on port {PORT}')
